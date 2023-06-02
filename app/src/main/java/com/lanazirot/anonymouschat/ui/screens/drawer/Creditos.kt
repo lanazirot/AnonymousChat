438e1c0a4ce8c3d20bc493b78f2c628f96425d7d
Ezequiel-Cantu <70524684+Ezequiel-Cantu@users.noreply.github.com>
2023-06-02 02:00:38 -0500

    Credits

--- app/src/main/java/com/lanazirot/anonymouschat/ui/screens/drawer/Creditos.kt
+++ app/src/main/java/com/lanazirot/anonymouschat/ui/screens/drawer/Creditos.kt
@@ -1,6 +1,6 @@
 package com.lanazirot.anonymouschat.ui.screens.drawer
 
-import android.os.Build
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,15 +8,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Computer
 import androidx.compose.material.icons.outlined.DesignServices
 import androidx.compose.material.icons.outlined.Notes
@@ -25,7 +25,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,13 +33,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.ImageLoader
-import coil.compose.rememberAsyncImagePainter
-import coil.decode.GifDecoder
-import coil.decode.ImageDecoderDecoder
-import coil.request.ImageRequest
-import coil.size.Size
-import com.lanazirot.anonymouschat.MainApp.Companion.context
 import com.lanazirot.anonymouschat.R
 import com.lanazirot.anonymouschat.ui.components.common.TopBar
 import com.lanazirot.anonymouschat.ui.providers.GlobalProvider
@@ -66,11 +58,27 @@ fun Creditos() {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.Start
         ) {
+            Column(modifier = Modifier
+                .fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                if(isDarkThemeEnabled){
+                    Image(
+                        painter = painterResource(id = R.drawable.logo), contentDescription = "", modifier =
+                        Modifier
+                            .width(150.dp)
+                            .height(100.dp)
+                    )
+                }else{
+
+                }
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
@@ -84,29 +92,33 @@ fun Creditos() {
                     color = MaterialTheme.colors.secondary,
                     fontFamily = Anonymous,
                     fontWeight = FontWeight.Black,
-                    fontSize = 35.sp,
-                    modifier = Modifier.padding(15.dp)
+                    fontSize = 30.sp,
+                    modifier = Modifier.padding(15.dp),
+                    textAlign = TextAlign.Right
                 )
             }
-            Text(
-                text = "Alan Peña Ortiz",
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.body1,
-                fontSize = 20.sp,
-                modifier = Modifier.padding(10.dp),
-                textAlign = TextAlign.Right
-            )
-            Text(
-                text = "Alan Castro Cruz",
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.body1,
-                fontSize = 20.sp,
-                modifier = Modifier.padding(10.dp),
-                textAlign = TextAlign.Right
-            )
+            Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End) {
+                Text(
+                    text = "Alan Peña Ortiz",
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.body1,
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(10.dp),
+                    textAlign = TextAlign.Right
+                )
+                Text(
+                    text = "Alan Castro Cruz",
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.body1,
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(10.dp),
+                    textAlign = TextAlign.Right
+                )
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
@@ -114,7 +126,7 @@ fun Creditos() {
                     color = MaterialTheme.colors.secondary,
                     fontFamily = Anonymous,
                     fontWeight = FontWeight.Black,
-                    fontSize = 35.sp,
+                    fontSize = 30.sp,
                     modifier = Modifier.padding(15.dp),
                     textAlign = TextAlign.Left
                 )
@@ -129,7 +141,7 @@ fun Creditos() {
                 text = "Ezequiel Cantu",
                 color = MaterialTheme.colors.primary,
                 style = MaterialTheme.typography.body1,
-                fontSize = 20.sp,
+                fontSize = 25.sp,
                 modifier = Modifier.padding(10.dp),
                 textAlign = TextAlign.Left
             )
@@ -137,20 +149,20 @@ fun Creditos() {
                 text = "Andrea Martinez",
                 color = MaterialTheme.colors.primary,
                 style = MaterialTheme.typography.body1,
-                fontSize = 20.sp,
+                fontSize = 25.sp,
                 modifier = Modifier.padding(10.dp),
                 textAlign = TextAlign.Left
             )
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.End,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
                     imageVector = Icons.Outlined.Notes,
                     contentDescription = "",
                     tint = MaterialTheme.colors.secondary,
-                    modifier = Modifier.size(30.dp)
+                    modifier = Modifier.size(35.dp)
                 )
                 Text(
                     text = " Testing ",
@@ -162,14 +174,17 @@ fun Creditos() {
                     textAlign = TextAlign.Right
                 )
             }
-            Text(
-                text = "Eduardo Jared",
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.body1,
-                fontSize = 20.sp,
-                modifier = Modifier.padding(10.dp),
-                textAlign = TextAlign.Right
-            )
+            Column(modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.End) {
+                Text(
+                    text = "Eduardo Jared",
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.body1,
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(10.dp),
+                    textAlign = TextAlign.Right
+                )
+            }
         }
     }
 }
