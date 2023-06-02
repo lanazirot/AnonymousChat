d8387a17e94b9959383280ee2bc3d1bc48b90ba7
Alan Peña Ortiz <alancin7@live.com.mx>
2023-06-02 01:40:33 -0500

    code: security button

--- app/src/main/java/com/lanazirot/anonymouschat/ui/screens/chat/components/ChatHeader.kt
+++ app/src/main/java/com/lanazirot/anonymouschat/ui/screens/chat/components/ChatHeader.kt
@@ -1,12 +1,12 @@
 package com.lanazirot.anonymouschat.ui.screens.chat.components
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -14,37 +14,63 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Chat
 import androidx.compose.material.icons.outlined.PeopleOutline
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.lanazirot.anonymouschat.R
+import com.lanazirot.anonymouschat.ui.screens.chat.ChatViewModel
 import com.lanazirot.anonymouschat.ui.theme.Anonymous
 import io.getstream.chat.android.client.models.Channel
 
 @Composable
-fun ChatHeader(channel: Channel, onBack : () -> Unit) {
+fun ChatHeader(channel: Channel, onBack: () -> Unit) {
+
+    val chatViewModel: ChatViewModel = hiltViewModel()
+    val chatState = chatViewModel.chatState.collectAsState().value
+    val context = LocalContext.current
+
+    LaunchedEffect(chatState.reported) {
+        if (!chatState.reported) {
+            Toast.makeText(
+                context,
+                "Room was reported to AnonymousChat team. Thank you! and keep safe.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }else{
+            Toast.makeText(
+                context,
+                "Room was already reported to AnonymousChat team. Thank you! and keep safe.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .background(MaterialTheme.colors.primaryVariant)
     ) {
-        Row (horizontalArrangement = Arrangement.Center,
+        Row(
+            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Button(
                 onClick = {
-                onBack()
-            }) {
+                    onBack()
+                }) {
                 Icon(
                     imageVector = Icons.Outlined.ArrowBack,
                     contentDescription = stringResource(R.string.back),
@@ -69,11 +95,16 @@ fun ChatHeader(channel: Channel, onBack : () -> Unit) {
                 ),
             )
             Spacer(modifier = Modifier.width(10.dp))
-            Icon(
-                imageVector = Icons.Outlined.Warning,
-                contentDescription = "",
-                tint = Color.Yellow
-            )
+            Button(
+                onClick = {
+                    chatViewModel.reportRoom()
+                }) {
+                Icon(
+                    imageVector = Icons.Outlined.Warning,
+                    contentDescription = "Report",
+                    tint = Color.Yellow
+                )
+            }
         }
     }
 }
